878ae584beae01b0183c1de6e844e486a3b25bbc
Daria <dariamesesan@yahoo.com>
2023-03-17 10:49:30 +0200

    Created the function for computer choice and linked .js to html

---
+++ game.js
@@ -0,0 +1,7 @@
+
+var options = ["rock", "paper", "scissors"];
+function getComputerChoice(){
+    return options[Math.floor(Math.random()*options.length)];
+}
+
+    console.log(getComputerChoice());
