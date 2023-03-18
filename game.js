bb1908c7800a5b4aadb6f0cbd3270cddf63e6c74
Daria <dariamesesan@yahoo.com>
2023-03-18 02:59:46 +0200

    Changed alt for images in order to be used in the javascript function

--- game.js
+++ game.js
@@ -8,28 +8,30 @@ function getComputerChoice() {
 
 
 function playRound(ComputerSelection, PlayerSelection) {
-    if (ComputerSelection == PlayerSelection)
-        return ("It's a draw");
+    if (ComputerSelection === PlayerSelection){
+        return "It's a draw";
+    }
+        
 
     if (ComputerSelection == "rock") {
         if (PlayerSelection == "paper")
-            return ("You won! Paper beats Rock!");
+            return "You won! Paper beats Rock!";
         else if (PlayerSelection == "scissors")
-            return ("You lost! Rock beats Scissors!");
+            return "You lost! Rock beats Scissors!";
 
     }
     if (ComputerSelection == "paper") {
         if (PlayerSelection == "rock")
-            return ("You lost! Paper beats Rock!");
+            return "You lost! Paper beats Rock!";
         if (PlayerSelection == "scissors")
-            return ("You won! Scissors beats Paper!");
+            return "You won! Scissors beats Paper!";
     }
 
     if (ComputerSelection == "scissors") {
         if (PlayerSelection == "rock")
-            return ("You won! Rock beats Scissors!");
+            return "You won! Rock beats Scissors!";
         else if (PlayerSelection == "paper")
-            return ("You lost! Scissors beats Paper!");
+            return "You lost! Scissors beats Paper!";
     }
 
 }
@@ -39,29 +41,32 @@ document.querySelectorAll('.choice_img').forEach(button => {
     button.addEventListener('click', () => {
         //read player choice
         let PlayerSelection = button.querySelector('img').alt.toLowerCase();
-
+        console.log(PlayerSelection);
         //generate computer choice
         let ComputerSelection = getComputerChoice();
-
+        console.log(ComputerSelection);
         //play round for the current player selection
         let result = playRound(ComputerSelection, PlayerSelection);
 
         //update points
-        let P_points = document.getElementById('PlayerPoits');
+        let P_points = document.getElementById('PlayerPoints');
         let C_points = document.getElementById('ComputerPoints');
         //convert the text to int to use for mathematical operations
         let PlayerPoints = parseInt(P_points.textContent);
         let ComputerPoints = parseInt(C_points.textContent);
+
+
         //check the result to update points
-        if(result.includes("You won!")){
+        if (result && result.includes("You won!")) {
             PlayerPoints++;
+            console.log(PlayerPoints);
             //go back to string format
-            P_points.textcontent = PlayerPoints.toString();
+            P_points.textContent = PlayerPoints.toString();
         }
-        if(result.includes("You lsot!")){
+        if (result && result.includes("You lost!")) {
             ComputerPoints++;
             //go back to string format
-            C_points.textcontent = ComputerPoints.toString();
+            C_points.textContent = ComputerPoints.toString();
         }
 
         //update round
@@ -75,21 +80,20 @@ document.querySelectorAll('.choice_img').forEach(button => {
         result_Element.textContent = result; //the result is generated after the playRound function
 
         //check if game is over
-        if(round_number == 5)
-        {
+        if (round_number == 5) {
             let GameOver = document.getElementById('game_over');
             let game_over_result_elem = document.getElementById('game_over_result');
             GameOver.style.display = 'block';
-            if(PlayerPoints > ComputerPoints)
-                GameOver.textContent = "You win";
-            if(ComputerPoints > PlayerPoints)
-            GameOver.textContent = "The computer wins";
-            if(ComputerPoints == PlayerPoints)
-            GameOver.textContent = "Equal scores - it's a draw";
+            if (PlayerPoints > ComputerPoints) {
+                game_over_result_elem.textContent = "You win";
+            } else if (ComputerPoints > PlayerPoints) {
+                game_over_result_elem.textContent = "The computer wins";
+            } else
+                game_over_result_elem.textContent = "Equal scores - it's a draw";
         }
-    }) 
-})
-   
+    })
+});
+
 
 // function game(computerSelection) {
 //     let PlayerPoints = 0;
@@ -98,7 +102,7 @@ document.querySelectorAll('.choice_img').forEach(button => {
 //     for (let i = 0; i < 5; i++) {
 //         //Read player option
 //         let player = prompt("Please enter rock/paper/scissors:"); //This entering will be case sensitive
-//         let playerSelection = player.toLowerCase(); //this makes the entering case insensitive by lowecasing the input 
+//         let playerSelection = player.toLowerCase(); //this makes the entering case insensitive by lowecasing the input
 
 //         //Generate computer option
 //         const computerSelection = getComputerChoice();
@@ -115,7 +119,7 @@ document.querySelectorAll('.choice_img').forEach(button => {
 //             ComputerPoints++;
 
 //         //print current result
-//         console.log("Round: " + (i+1) + " Now the score is: Player - " + 
+//         console.log("Round: " + (i+1) + " Now the score is: Player - " +
 //         PlayerPoints + " Computer - " + ComputerPoints);
 //     }
 //     //print final result
